7c6e5c1657eead5b16afc81fd9b3fcac992ecf79
zhanglu1 <3402257924@qq.com>
2025-11-28 17:18:36 +0800

    挂载

--- src/components/Plugins/MessageContent/index.vue
+++ src/components/Plugins/MessageContent/index.vue
@@ -4,5 +4,8 @@
 <script lang="ts" setup>
 import { ElMessage } from 'element-plus'
 //挂载在 window 方便与在js中使用
+// 它唯一的作用：把 Element Plus 的 ElMessage 挂到全局 window 上，让项目里的纯 JS 文件也能使用消息提示
+// 可以直接使用 ElMessage.success('操作成功')
+// 这些文件不是 Vue 组件实例环境中，你直接用 ElMessage 可能会报错，又或者你不想在每个文件里都 import Element Plus。
 window['$message'] = ElMessage
 </script>
